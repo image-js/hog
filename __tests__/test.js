28562980bf53ebf9f152e4a2e1137a7b8a0e5db9
Michaël Zasso <targos@protonmail.com>
2018-05-23 17:02:30 +0200

    remove unused dependencies

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,22 +1,23 @@
 'use strict';
 
 const path = require('path');
-const {Image} = require('image-js');
+
+const { Image } = require('image-js');
 
 const HOG = require('../src');
 
 describe('hog size test', () => {
-    it('Main test', async () => {
-        const image = await Image.load(path.join(__dirname, 'beachball.png'));
-        const sizeImage = 110; // image test1/png is square sizeImage*sizeImage
-        const options = {
-            cellSize: 4,
-            bins: 6,
-            blockSize: 2,
-            blockStride: 1
-        };
-        const descriptorSize = HOG.extractHOG(image, options).length;
-        const sizeExpected = (Math.floor((parseFloat(sizeImage) / options.cellSize - options.blockSize) / (options.blockSize - options.blockStride)) + 1) ** 2 * options.bins * options.cellSize;
-        expect(descriptorSize).toEqual(sizeExpected);
-    });
+  it('Main test', async () => {
+    const image = await Image.load(path.join(__dirname, 'beachball.png'));
+    const sizeImage = 110; // image test1/png is square sizeImage*sizeImage
+    const options = {
+      cellSize: 4,
+      bins: 6,
+      blockSize: 2,
+      blockStride: 1
+    };
+    const descriptorSize = HOG.extractHOG(image, options).length;
+    const sizeExpected = (Math.floor((parseFloat(sizeImage) / options.cellSize - options.blockSize) / (options.blockSize - options.blockStride)) + 1) ** 2 * options.bins * options.cellSize;
+    expect(descriptorSize).toEqual(sizeExpected);
+  });
 });
