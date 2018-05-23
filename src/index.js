25f36ab3b66312632879a2bbe2e111903e918113
Michaël Zasso <targos@protonmail.com>
2018-05-23 16:57:59 +0200

    chore: update dependencies

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const processing = require('./processingHog');
 const PI_RAD = 180 / Math.PI;
 
 module.exports = {
-    extractHOG: extractHOG,
-    extractHistograms: extractHistograms
+  extractHOG: extractHOG,
+  extractHistograms: extractHistograms
 };
 
 // also export all the functions from processing.js
 for (var func in processing) {
-    module.exports[func] = processing[func];
+  module.exports[func] = processing[func];
 }
 
 /**
@@ -22,26 +22,26 @@ for (var func in processing) {
  */
 
 function extractHOG(image, options = {}) {
-    const {
-        blockSize = 2,
-        blockStride = blockSize / 2,
-        norm = 'L2'
-    } = options;
+  const {
+    blockSize = 2,
+    blockStride = blockSize / 2,
+    norm = 'L2'
+  } = options;
 
-    var histograms = extractHistograms(image, options);
+  var histograms = extractHistograms(image, options);
 
-    var blocks = [];
-    var blocksHigh = histograms.length - blockSize + 1;
-    var blocksWide = histograms[0].length - blockSize + 1;
+  var blocks = [];
+  var blocksHigh = histograms.length - blockSize + 1;
+  var blocksWide = histograms[0].length - blockSize + 1;
 
-    for (var y = 0; y < blocksHigh; y += blockStride) {
-        for (var x = 0; x < blocksWide; x += blockStride) {
-            var block = getBlock(histograms, x, y, blockSize);
-            normalize(block, norm);
-            blocks.push(block);
-        }
+  for (var y = 0; y < blocksHigh; y += blockStride) {
+    for (var x = 0; x < blocksWide; x += blockStride) {
+      var block = getBlock(histograms, x, y, blockSize);
+      normalize(block, norm);
+      blocks.push(block);
     }
-    return Array.prototype.concat.apply([], blocks);
+  }
+  return Array.prototype.concat.apply([], blocks);
 }
 
 /**
@@ -52,25 +52,25 @@ function extractHOG(image, options = {}) {
  */
 
 function extractHistograms(image, options = {}) {
-    var vectors = processing.gradientVectors(image);
+  var vectors = processing.gradientVectors(image);
 
-    var cellSize = options.cellSize || 4;
-    var bins = options.bins || 6;
+  var cellSize = options.cellSize || 4;
+  var bins = options.bins || 6;
 
-    var cellsWide = Math.floor(vectors[0].length / cellSize);
-    var cellsHigh = Math.floor(vectors.length / cellSize);
+  var cellsWide = Math.floor(vectors[0].length / cellSize);
+  var cellsHigh = Math.floor(vectors.length / cellSize);
 
-    var histograms = new Array(cellsHigh);
+  var histograms = new Array(cellsHigh);
 
-    for (var i = 0; i < cellsHigh; i++) {
-        histograms[i] = new Array(cellsWide);
+  for (var i = 0; i < cellsHigh; i++) {
+    histograms[i] = new Array(cellsWide);
 
-        for (var j = 0; j < cellsWide; j++) {
-            histograms[i][j] = getHistogram(vectors, j * cellSize, i * cellSize,
-                cellSize, bins);
-        }
+    for (var j = 0; j < cellsWide; j++) {
+      histograms[i][j] = getHistogram(vectors, j * cellSize, i * cellSize,
+        cellSize, bins);
     }
-    return histograms;
+  }
+  return histograms;
 }
 
 /**
@@ -83,13 +83,13 @@ function extractHistograms(image, options = {}) {
  */
 
 function getBlock(matrix, x, y, length) {
-    var square = [];
-    for (var i = y; i < y + length; i++) {
-        for (var j = x; j < x + length; j++) {
-            square.push(matrix[i][j]);
-        }
+  var square = [];
+  for (var i = y; i < y + length; i++) {
+    for (var j = x; j < x + length; j++) {
+      square.push(matrix[i][j]);
     }
-    return Array.prototype.concat.apply([], square);
+  }
+  return Array.prototype.concat.apply([], square);
 }
 
 /**
@@ -103,29 +103,29 @@ function getBlock(matrix, x, y, length) {
  */
 
 function getHistogram(elements, x, y, size, bins) {
-    var histogram = new Array(bins).fill(0);
+  var histogram = new Array(bins).fill(0);
 
-    for (var i = 0; i < size; i++) {
-        for (var j = 0; j < size; j++) {
-            var vector = elements[y + i][x + j];
-            var bin = binFor(vector.orient, bins);
-            histogram[bin] += vector.mag;
-        }
+  for (var i = 0; i < size; i++) {
+    for (var j = 0; j < size; j++) {
+      var vector = elements[y + i][x + j];
+      var bin = binFor(vector.orient, bins);
+      histogram[bin] += vector.mag;
     }
-    return histogram;
+  }
+  return histogram;
 }
 
 function binFor(radians, bins) {
-    var angle = radians * (PI_RAD);
-    if (angle < 0) {
-        angle += 180;
-    }
+  var angle = radians * (PI_RAD);
+  if (angle < 0) {
+    angle += 180;
+  }
 
-    // center the first bin around 0
-    angle += 90 / bins;
-    angle %= 180;
+  // center the first bin around 0
+  angle += 90 / bins;
+  angle %= 180;
 
-    return Math.floor(angle / 180 * bins);
+  return Math.floor(angle / 180 * bins);
 }
 
 /**
@@ -135,36 +135,36 @@ function binFor(radians, bins) {
  */
 
 function normalize(vector, norm) {
-    var epsilon = 0.00001;
-    var sum, denom, i;
-    if (norm === 'L1') {
-        sum = 0;
-        for (i = 0; i < vector.length; i++) {
-            sum += Math.abs(vector[i]);
-        }
-        denom = sum + epsilon;
-
-        for (i = 0; i < vector.length; i++) {
-            vector[i] /= denom;
-        }
-    } else if (norm === 'L1-sqrt') {
-        sum = 0;
-        for (i = 0; i < vector.length; i++) {
-            sum += Math.abs(vector[i]);
-        }
-        denom = sum + epsilon;
-
-        for (i = 0; i < vector.length; i++) {
-            vector[i] = Math.sqrt(vector[i] / denom);
-        }
-    } else { // i.e norm === "L2"
-        sum = 0;
-        for (i = 0; i < vector.length; i++) {
-            sum += vector[i] * vector[i];
-        }
-        denom = Math.sqrt(sum + epsilon);
-        for (i = 0; i < vector.length; i++) {
-            vector[i] /= denom;
-        }
+  var epsilon = 0.00001;
+  var sum, denom, i;
+  if (norm === 'L1') {
+    sum = 0;
+    for (i = 0; i < vector.length; i++) {
+      sum += Math.abs(vector[i]);
     }
+    denom = sum + epsilon;
+
+    for (i = 0; i < vector.length; i++) {
+      vector[i] /= denom;
+    }
+  } else if (norm === 'L1-sqrt') {
+    sum = 0;
+    for (i = 0; i < vector.length; i++) {
+      sum += Math.abs(vector[i]);
+    }
+    denom = sum + epsilon;
+
+    for (i = 0; i < vector.length; i++) {
+      vector[i] = Math.sqrt(vector[i] / denom);
+    }
+  } else { // i.e norm === "L2"
+    sum = 0;
+    for (i = 0; i < vector.length; i++) {
+      sum += vector[i] * vector[i];
+    }
+    denom = Math.sqrt(sum + epsilon);
+    for (i = 0; i < vector.length; i++) {
+      vector[i] /= denom;
+    }
+  }
 }
--- src/processingHog.js
+++ src/processingHog.js
@@ -1,81 +1,81 @@
 'use strict';
 
 function intensities(image) {
-    if (image.components === 1) {
-        return image;
-    } else {
-        return image.grey({algorithm: 'luma601'});
-    }
+  if (image.components === 1) {
+    return image;
+  } else {
+    return image.grey({ algorithm: 'luma601' });
+  }
 }
 
 function gradients(imagedata) {
-    return _gradients(intensities(imagedata));
+  return _gradients(intensities(imagedata));
 }
 
 function _gradients(intensities) {
-    const height = intensities.height;
-    const width = intensities.width;
-    const maxValue = intensities.maxValue;
+  const height = intensities.height;
+  const width = intensities.width;
+  const maxValue = intensities.maxValue;
 
-    const gradX = new Array(height);
-    const gradY = new Array(height);
+  const gradX = new Array(height);
+  const gradY = new Array(height);
 
-    for (var y = 0; y < height; y++) {
-        gradX[y] = new Array(width);
-        gradY[y] = new Array(width);
+  for (var y = 0; y < height; y++) {
+    gradX[y] = new Array(width);
+    gradY[y] = new Array(width);
 
-        for (var x = 0; x < width; x++) {
-            var prevX = x === 0 ? 0 : intensities.getValueXY(x - 1, y, 1) / maxValue;
-            var nextX = x === width - 1 ? 0 : intensities.getValueXY(x + 1, y, 1) / maxValue;
-            var prevY = y === 0 ? 0 : intensities.getValueXY(x, y - 1, 1) / maxValue;
-            var nextY = y === height - 1 ? 0 : intensities.getValueXY(x, y + 1, 1) / maxValue;
+    for (var x = 0; x < width; x++) {
+      var prevX = x === 0 ? 0 : intensities.getValueXY(x - 1, y, 1) / maxValue;
+      var nextX = x === width - 1 ? 0 : intensities.getValueXY(x + 1, y, 1) / maxValue;
+      var prevY = y === 0 ? 0 : intensities.getValueXY(x, y - 1, 1) / maxValue;
+      var nextY = y === height - 1 ? 0 : intensities.getValueXY(x, y + 1, 1) / maxValue;
 
-            // kernel [-1, 0, 1]
-            gradX[y][x] = -prevX + nextX;
-            gradY[y][x] = -prevY + nextY;
-        }
+      // kernel [-1, 0, 1]
+      gradX[y][x] = -prevX + nextX;
+      gradY[y][x] = -prevY + nextY;
     }
+  }
 
-    return {
-        x: gradX,
-        y: gradY
-    };
+  return {
+    x: gradX,
+    y: gradY
+  };
 }
 
 function gradientVectors(image) {
-    return _gradientVectors(intensities(image));
+  return _gradientVectors(intensities(image));
 }
 
 function _gradientVectors(intensities) {
-    const height = intensities.height;
-    const width = intensities.width;
-    const maxValue = intensities.maxValue;
+  const height = intensities.height;
+  const width = intensities.width;
+  const maxValue = intensities.maxValue;
 
-    const vectors = new Array(height);
+  const vectors = new Array(height);
 
-    for (var y = 0; y < height; y++) {
-        vectors[y] = new Array(width);
-        for (var x = 0; x < width; x++) {
-            var prevX = x === 0 ? 0 : intensities.getValueXY(x - 1, y, 0) / maxValue;
-            var nextX = x === width - 1 ? 0 : intensities.getValueXY(x + 1, y, 0) / maxValue;
-            var prevY = y === 0 ? 0 : intensities.getValueXY(x, y - 1, 0) / maxValue;
-            var nextY = y === height - 1 ? 0 : intensities.getValueXY(x, y + 1, 0) / maxValue;
+  for (var y = 0; y < height; y++) {
+    vectors[y] = new Array(width);
+    for (var x = 0; x < width; x++) {
+      var prevX = x === 0 ? 0 : intensities.getValueXY(x - 1, y, 0) / maxValue;
+      var nextX = x === width - 1 ? 0 : intensities.getValueXY(x + 1, y, 0) / maxValue;
+      var prevY = y === 0 ? 0 : intensities.getValueXY(x, y - 1, 0) / maxValue;
+      var nextY = y === height - 1 ? 0 : intensities.getValueXY(x, y + 1, 0) / maxValue;
 
-            // kernel [-1, 0, 1]
-            var gradX = -prevX + nextX;
-            var gradY = -prevY + nextY;
+      // kernel [-1, 0, 1]
+      var gradX = -prevX + nextX;
+      var gradY = -prevY + nextY;
 
-            vectors[y][x] = {
-                mag: Math.sqrt(Math.pow(gradX, 2) + Math.pow(gradY, 2)),
-                orient: Math.atan2(gradY, gradX)
-            };
-        }
+      vectors[y][x] = {
+        mag: Math.sqrt(Math.pow(gradX, 2) + Math.pow(gradY, 2)),
+        orient: Math.atan2(gradY, gradX)
+      };
     }
-    return vectors;
+  }
+  return vectors;
 }
 
 module.exports = {
-    intensities,
-    gradients,
-    gradientVectors
+  intensities,
+  gradients,
+  gradientVectors
 };
